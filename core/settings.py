d0e57c503e47320ab64a20ea5061792f2dc01d1d
bitcore <rfmiranda27@gmail.com>
2026-09-16 16:57:08 -0300

    2026.09.16

--- core/settings.py
+++ core/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!p4nt#=0u6j$$9sl^05&+g%dn#dbc9*^*4!zsk8-jrz&6d_+3$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]']
 
